24c0f7f7c9d28ba8cc77f5fa0cd3e5e0560794f9
Matthew Curtis <matthewcurtis@khanacademy.org>
2024-06-25 09:00:08 -0500

    refactor with better solution

--- midi-test.lua
+++ midi-test.lua
@@ -1,16 +1,47 @@
 pressed_notes = {}
+midi_devices = {}
 
 active_list_item = 1
 
-in_midi_index = 1
-in_midi_channel = 1
 in_midi_connection = nil
-out_midi_index = 1
-out_midi_channel = 1
 out_midi_connection = nil
 
 function init()
+  build_midi_device_list()
+
+  params:add_option("midi_in_device", "midi in device", midi_devices, 1)
+  params:set_action("midi_in_device", function() setup_midi_callback() end)
+   
+  params:add_number("midi_in_channel", "midi in channel", 1, 16, 1)
+  params:set_action("midi_in_channel", function() setup_midi_callback() end)
+
+  params:add_option("midi_out_device", "midi out device", midi_devices, 1)
+  params:set_action("midi_out_device", function()  setup_midi_callback() end)
+
+  params:add_number("midi_out_channel", "midi out channel", 1, 16, 1)
+  params:set_action("midi_out_channel", function() setup_midi_callback() end)
+
   setup_midi_callback()
+end
+
+function build_midi_device_list()
+  midi_devices = {}
+  for i = 1, #midi.vports do
+    local long_name = midi.vports[i].name
+    local short_name = string.len(long_name) > 15 and util.acronym(long_name) or long_name
+    table.insert(midi_devices, short_name)
+  end
+end
+
+function midi.add()
+  build_midi_device_list()
+end
+
+function midi.remove()
+  clock.run(function()
+    clock.sleep(0.2)
+    build_midi_device_list()
+  end)
 end
 
 function setup_midi_callback()
@@ -18,11 +49,14 @@ function setup_midi_callback()
   if out_midi_connection ~= nil then
     stop_all_notes()
   end
-  midi.cleanup()
+
+  for i = 1, 16 do
+    midi.vports[i].event = nil
+  end
 
   -- make new connections
-  in_midi_connection = midi.connect(in_midi_index)
-  out_midi_connection = midi.connect(out_midi_index)
+  in_midi_connection = midi.connect(params:get("midi_in_device"))
+  out_midi_connection = midi.connect(params:get("midi_out_device"))
 
   -- listen for note on/off
   in_midi_connection.event = function(data)
@@ -30,7 +64,7 @@ function setup_midi_callback()
     tab.print(message)
     print(" ")
   
-    if (message.ch == in_midi_channel) then
+    if (message.ch == params:get("midi_in_channel")) then
       if message.type == "note_on" then
         pressed_notes[message.note] = message.note
         out_midi_connection:note_on(
@@ -42,7 +76,7 @@ function setup_midi_callback()
         out_midi_connection:note_off(
           message.note,
           100,
-          out_midi_channel)
+          params:get("midi_out_channel"))
       end
     end
   
@@ -88,10 +122,13 @@ function redraw()
   screen.clear()
   screen.fill()
 
-  draw_line(0, "in:", in_midi_index.." "..midi.devices[in_midi_index].name, active_list_item==1)
-  draw_line(10, "in ch:", in_midi_channel, active_list_item==2)
-  draw_line(20, "out:", out_midi_index .." "..midi.devices[out_midi_index].name, active_list_item==3)
-  draw_line(30, "out ch:", out_midi_channel, active_list_item==4)
+  in_midi_index = params:get("midi_in_device")
+  draw_line(0, "in:", in_midi_index.." "..midi_devices[in_midi_index], active_list_item==1)
+  draw_line(10, "in ch:", params:get("midi_in_channel"), active_list_item==2)
+
+  out_midi_index = params:get("midi_out_device")
+  draw_line(20, "out:", out_midi_index .." "..midi_devices[out_midi_index], active_list_item==3)
+  draw_line(30, "out ch:", params:get("midi_out_channel"), active_list_item==4)
 
   if has_note_on() then
     screen.level(15)
@@ -113,19 +150,17 @@ function enc(n,d)
   elseif n == 3 then
     if (active_list_item == 1) then
       -- MIDI in device
-      in_midi_index = util.clamp(in_midi_index + d, 1, #midi.devices)
+      params:set("midi_in_device", params:get("midi_in_device") + d)
     elseif (active_list_item == 2) then
       -- MIDI in channel
-      in_midi_channel = util.clamp(in_midi_channel + d, 1, 16)
+      params:set("midi_in_channel", params:get("midi_in_channel") + d)
     elseif (active_list_item == 3) then
       -- MIDI out device
-      out_midi_index = util.clamp(out_midi_index + d, 1, #midi.devices)
+      params:set("midi_out_device", params:get("midi_out_device") + d)
     elseif (active_list_item == 4) then
       -- MIDI out channel
-      out_midi_channel = util.clamp(out_midi_channel + d, 1, 16)
+      params:set("midi_out_channel", params:get("midi_out_channel") + d)
     end
-
-    setup_midi_callback()
   end
 
   redraw()
